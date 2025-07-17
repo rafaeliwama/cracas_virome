65dcb0eb4681c98012dc14d1f88f6c1744e8364f
rafaeliwama <eiji.iwama@gmail.com>
2025-07-17 08:12:57 -0300

    Created: 2023-10-30 12:00:00 added: Remove raw files after processing

--- kaiju.sh
+++ kaiju.sh
@@ -14,4 +14,21 @@
 ## fix files from sortmerna with BBmap repair.sh
 conda activate BBmap
 
-repair.sh in1=~/Cracas_viroma/sortmerna_done/ in2=~/Cracas_viroma/sortmerna_done/*.PE2_Ced.fastq out1=~/Cracas_viroma/sortmerna_done/paired.PE1_Ced.fastq out2=~/Cracas_viroma/sortmerna_done/paired.PE2_Ced.fastq
+for FILE in *_PE1.fastq; do
+  BASENAME="${FILE/_PE1.fastq/}"
+  repair.sh in1="$FILE" in2="${BASENAME}_PE2.fastq" out1="${BASENAME}_PE1_fixed.fastq" out2="${BASENAME}_PE2_fixed.fastq"
+done
+
+conda deactivate BBmap
+
+## runs Kaiju to classify the reads
+conda activate kaiju
+
+for FILE in *_PE1.fastq; do
+    BASENAME="${FILE/_PE1_fixed.fastq/}"
+    kaiju -E 10-5 -v -z 16 -t ~/databases/kaijudb/nodes.dmp -f ~/databases/kaijudb/viruses/kaiju_db_viruses.fmi -i $FILE -j "${BASENAME}_PE2_fixed.fq" -o $BASENAME.kaiju
+done
+
+conda deactivate kaiju
+
+
