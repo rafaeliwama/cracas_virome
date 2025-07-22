e093d8021e3393d20856a1b3f1852bbceadb08ab
rafaeliwama <eiji.iwama@gmail.com>
2025-07-22 12:00:31 -0300

    test

--- kaiju.sh
+++ kaiju.sh
@@ -15,7 +15,7 @@
 conda activate BBmap
 
 for FILE in *_PE1.fastq; do
-  BASENAME="${FILE/_PE1.fastq/}"
+  BASENAME="${FILE/_PE1\.fastq/}"
   repair.sh in1="$FILE" in2="${BASENAME}_PE2.fastq" out1="${BASENAME}_PE1_fixed.fastq" out2="${BASENAME}_PE2_fixed.fastq"
 done
 
@@ -25,7 +25,7 @@ conda deactivate BBmap
 conda activate kaiju
 
 for FILE in *_PE1.fastq; do
-    BASENAME="${FILE/_PE1_fixed.fastq/}"
+    BASENAME="${FILE/_PE1_fixed\.fastq/}"
     kaiju -E 10-5 -v -z 16 -t ~/databases/kaijudb/nodes.dmp -f ~/databases/kaijudb/viruses/kaiju_db_viruses.fmi -i $FILE -j "${BASENAME}_PE2_fixed.fq" -o $BASENAME.kaiju
 done
 
