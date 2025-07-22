9f9bbffb5da6b07ea412fbd865e63d16d3c3d32c
rafaeliwama <eiji.iwama@gmail.com>
2025-07-22 12:03:23 -0300

    test

--- kaiju.sh
+++ kaiju.sh
@@ -14,8 +14,8 @@
 ## fix files from sortmerna with BBmap repair.sh
 conda activate BBmap
 
-for FILE in *_PE1.fastq; do
-  BASENAME="${FILE/_PE1\.fastq/}"
+for FILE in *_rev.fq; do
+  BASENAME="${FILE/_rev.fq/}"
   repair.sh in1="$FILE" in2="${BASENAME}_PE2.fastq" out1="${BASENAME}_PE1_fixed.fastq" out2="${BASENAME}_PE2_fixed.fastq"
 done
 
@@ -24,8 +24,8 @@ conda deactivate BBmap
 ## runs Kaiju to classify the reads
 conda activate kaiju
 
-for FILE in *_PE1.fastq; do
-    BASENAME="${FILE/_PE1_fixed\.fastq/}"
+for FILE in *_rev.fq; do
+    BASENAME="${FILE/_rev.fq/}"
     kaiju -E 10-5 -v -z 16 -t ~/databases/kaijudb/nodes.dmp -f ~/databases/kaijudb/viruses/kaiju_db_viruses.fmi -i $FILE -j "${BASENAME}_PE2_fixed.fq" -o $BASENAME.kaiju
 done
 
