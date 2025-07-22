dbe366e3304147e4d485993d12e7bea34a7b6dcb
rafaeliwama <eiji.iwama@gmail.com>
2025-07-22 06:56:04 -0300

    test

--- sortmerna.sh
+++ sortmerna.sh
@@ -23,6 +23,6 @@ cat temp1.tmp | sort | uniq > accession_list.tmp
 
 # run sortmerna for paired ended transcriptomes
 
-cat accession_list.tmp | while read line; do sortmerna -ref ~/databases/sortmerna_db/smr_v4.3_default_db.fasta -reads "${line}_PE1.fastq" -reads "${line}_PE2.fastq" -a 16 -other $line -out2 -fastx -workdir ~/Cracas_viroma/seqyclean_done/sort_wd; mv ~/Cracas_viroma/seqyclean_done/sort_wd/out/"${line}_fwd.fq" /home/riwama/Cracas_viroma/seqyclean_done/s_done; mv ~/Cracas_viroma/seqyclean_done/sort_wd/out/"${line}_rev.fq" /home/riwama/Cracas_viroma/seqyclean_done/s_done; done
+cat accession_list.tmp | while read line; do sortmerna -ref ~/databases/sortmerna_db/smr_v4.3_default_db.fasta -reads "${line}_PE1.fastq" -reads "${line}_PE2.fastq" -a 16 -other ~/Cracas_viroma/seqyclean_done/sort_wd/out/$line -out2 -fastx -workdir ~/Cracas_viroma/seqyclean_done/sort_wd; mv ~/Cracas_viroma/seqyclean_done/sort_wd/out/"${line}_fwd.fq" /home/riwama/Cracas_viroma/seqyclean_done/s_done; mv ~/Cracas_viroma/seqyclean_done/sort_wd/out/"${line}_rev.fq" /home/riwama/Cracas_viroma/seqyclean_done/s_done; done
 
 rm *.tmp
