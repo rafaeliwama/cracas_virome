3421b7783bfdb8fc39a3d545035e707ddbf419b7
rafaeliwama <eiji.iwama@gmail.com>
2025-07-22 11:56:16 -0300

    add mv command to move sorted files to a specific directory

--- sortmerna.sh
+++ sortmerna.sh
@@ -23,6 +23,8 @@ cat temp1.tmp | sort | uniq > accession_list.tmp
 
 # run sortmerna for paired ended transcriptomes
 
-cat accession_list.tmp | while read line; do sortmerna -ref ~/databases/sortmerna_db/smr_v4.3_default_db.fasta -reads "${line}_PE1.fastq" -reads "${line}_PE2.fastq" -a 16 -other ~/Cracas_viroma/seqyclean_done/sort_wd/out/$line -out2 -fastx -workdir ~/Cracas_viroma/seqyclean_done/sort_wd; mv ~/Cracas_viroma/seqyclean_done/sort_wd/out/"${line}_fwd.fq" /home/riwama/Cracas_viroma/seqyclean_done/s_done; mv ~/Cracas_viroma/seqyclean_done/sort_wd/out/"${line}_rev.fq" /home/riwama/Cracas_viroma/seqyclean_done/s_done; rm ~/Cracas_viroma/seqyclean_done/sort_wd/* -rf; done
+cat accession_list.tmp | while read line; do sortmerna -ref ~/databases/sortmerna_db/smr_v4.3_default_db.fasta -reads "${line}_PE1.fastq" -reads "${line}_PE2.fastq" -a 16 -other ~/Cracas_viroma/seqyclean_done/sort_wd/out/$line -out2 -fastx -workdir ~/Cracas_viroma/seqyclean_done/sort_wd; mv ~/Cracas_viroma/seqyclean_done/sort_wd/out/"${line}_fwd.fq" /home/riwama/Cracas_viroma/seqyclean_done/s_done; mv ~/Cracas_viroma/seqyclean_done/sort_wd/out/"${line}_rev.fq" /home/riwama/Cracas_viroma/seqyclean_done/s_done; rm ~/Cracas_viroma/seqyclean_done/sort_wd/* -rf; rm  done
 
 rm *.tmp
+
+mv s_done/*.fq ~/Cracas_viroma/sortmerna_done/
