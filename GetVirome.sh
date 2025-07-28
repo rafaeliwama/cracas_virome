472fd92184120a5b27efec9e848611c3e372d7f6
rafaeliwama <eiji.iwama@gmail.com>
2025-07-28 10:55:11 -0300

    test

--- GetVirome.sh
+++ GetVirome.sh
@@ -12,7 +12,7 @@ cat ../$1 | while read line; do
     fasterq-dump $line 
     seqyclean -1 "${line}_1.fastq" -2 "${line}_2.fastq" -qual -o $line -c /home/riwama/databases/univec/univec.fasta
     sortmerna -ref ~/databases/sortmerna_db/smr_v4.3_default_db.fasta -reads "${line}_PE1.fastq" -reads "${line}_PE2.fastq" -a 16 -other $line -out2 -fastx -workdir "${A}"/srtRNA_wd_tmp
-    repair.sh in1="${line}_fwd.fq" in2="${line}_rev.fq" out1="${line}_fwd_fixed.fastq" out2="${line}_rev_fixed.fastq"
+    repair.sh in1="srtRNA_wd_tmp/${line}_fwd.fq" in2="srtRNA_wd_tmp/${line}_rev.fq" out1="srtRNA_wd_tmp/${line}_fwd_fixed.fastq" out2="${line}_rev_fixed.fastq"
     kaiju -E 10-5 -v -z 16 -t ~/databases/kaijudb/nodes.dmp -f ~/databases/kaijudb/viruses/kaiju_db_viruses.fmi -i "${line}_fwd_fixed.fastq" -j "${line}_rev_fixed.fastq" -o ../$line.kaiju
     cd A=$(pwd)
     rm -rf wd_dir
