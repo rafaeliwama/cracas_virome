4ebf8226db1a4c76173947960a24c1d504e7598c
Rafael Eiji Iwama <46658489+rafaeliwama@users.noreply.github.com>
2025-08-20 16:13:58 +0200

    Update GetVirome.sh

--- to_sonia/GetVirome.sh
+++ to_sonia/GetVirome.sh
@@ -8,7 +8,7 @@ cat $1 | while read line; do
     cd wd_dir
     mkdir srtRNA_wd_tmp
     fasterq-dump $line -e 30
-    seqyclean -1 "${line}_1.fastq" -2 "${line}_2.fastq" -qual -o $line -c /home/riwama/databases/univec/univec.fasta -minlen 50
+    seqyclean -1 "${line}_1.fastq" -2 "${line}_2.fastq" -qual -o $line -c ~/databases/univec/univec.fasta -minlen 50
     echo "seqyclean done"
     sortmerna -ref ~/databases/sortmerna_db/smr_v4.3_default_db.fasta -reads "${line}_PE1.fastq" -reads "${line}_PE2.fastq" -a 30 -other ./$line -out2 -fastx -workdir srtRNA_wd_tmp
     echo "sortmerna done"
